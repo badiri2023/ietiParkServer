6752198c08682110266ea5ef28b6e4234dbfe3e8
Yessc <dianacarbob@gmail.com>
2026-04-27 15:56:37 +0200

    key

--- game/world.js
+++ game/world.js
@@ -9,7 +9,7 @@ class World {
 
         // PUERTA
         const doorSprite = level.sprites.find(s => s.type === "door");
-        const keySprite = level.sprites.find(s =>type ==="skeleton_key")
+)
 
         this.door = {
             x: doorSprite.x,
@@ -29,7 +29,7 @@ class World {
 
         //  OBSTÁCULOS (por ahora vacío)
         this.obstacles = [];
-
+        //key
         this.key = {
             x: 200,
             y: 300,
