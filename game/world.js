914649d06da913af4d611af5563a375869768606
Yessc <dianacarbob@gmail.com>
2026-05-04 19:07:29 +0200

    pull

--- game/world.js
+++ game/world.js
@@ -1,61 +1,105 @@
 const levelData = require('./game_data.json');
 class World {
-    constructor() {
-        const level = levelData.levels[0];
-        this.sprites = level.sprites;
-        this.layers = level.layers;
-
-        
-        //console.log("LEVEL:", level);
-        //console.log("SPRITES:", level.sprites);
-        //console.log("WORLD SIZE:", this.width, this.height);
-        // PUERTA
-        const doorSprite = level.sprites.find(s => s.type === "door");
-        //this.width = Math.max(...level.sprites.map(s => s.x + s.width));
-        //this.height = Math.max(...level.sprites.map(s => s.y + s.height));
-        this.width=800;
-        this.height=400;
-        
-        if (doorSprite) {
-            this.door = {
-                x: doorSprite.x,
-                y: doorSprite.y,
-                width: doorSprite.width,
-                height: doorSprite.height,
-                opened: false
-            };
-        } else {
-            console.warn("ya no hay puerta");
-            this.door = null; 
+    constructor(levelIndex = 0) {
+        this.levelIndex = levelIndex;
+        this.loadLevel(levelIndex);
+    }
+    loadLevel(index) {
+        const level = levelData.levels[index];
+        if (!level) {
+            console.error("Nivel no existe:", index);
+            return;
         }
+        
 
-        //  SPAWNS (jugadores)
+
+        this.currentLevel = level;
+        this.sprites = level.sprites || [];
+        this.layers = level.layers || [];
+
+        ///#####hablar con Bad para medidas del mundo por que el nivel 2 es mas grande
+        //tamaño mundo lo ajuste por que el viewport es muy pequeño 
+        //this.width = level.viewportWidth;
+        //this.height = level.viewportHeight;
+       //this.width = 1500;
+        //this.height = 600;
+        console.log("LEVEL:", level);
+        // reset total por nivel
+       
+
+        // tamaño mundo
+        this.width =  1500;
+        this.height =  600;
+
+
+        //-----Puerta-------
+        const doorSprite = level.sprites.find(s => s.type === "door");
+        this.door = doorSprite ? {
+            x: doorSprite.x,
+            y: 70,
+            width: doorSprite.width,
+            height: doorSprite.height,
+            opened: false
+        } : null;
+
+        ///------spawns------
         this.spawns = level.sprites
-            .filter(s => s.type === "player1")
+            .filter(s => s.type === "player1"|| s.type === "skeleton1")
             .map(s => ({
                 x: s.x,
                 y: s.y
             }));
 
-        //  OBSTÁCULOS (por ahora vacío)
-        this.obstacles = [];
-        //console.log("Mundo iniciado con éxito:");
-        //console.log("- Puerta en:", this.door.x, this.door.y);
-        //console.log("- Spawn en:", this.spawns[0]);
-        //key 13 gestiono estado key
+        //-----key-----
         const keySprite = level.sprites.find(s => s.type === "key");
-    
-        //console.log("Spawn encontrado:", level.sprites.find(s => s.type === "player1"));
-        
         this.key = {
             x: keySprite ? keySprite.x : 200,
             y:  keySprite ? (keySprite.y - 50) : 50,
             width: keySprite.width,
-            height: keySprite ? (keySprite.height + 80) : 112,            collected: false,
+            height: keySprite ? (keySprite.height + 80) : 112,            
+            collected: false,
             holderId: null
         };
-     
+
+        //palanca nivel 2
+
+
+        const palancaSprite = this.sprites.find(s => s.type === "palanca");
+        this.palanca = palancaSprite ? {
+            x: palancaSprite.x,
+            y: palancaSprite.y,
+            width: palancaSprite.width,
+            height: palancaSprite.height,
+            activated: false
+        } : null;
+
+
+            //  OBSTÁCULOS (por ahora vacío), para segundo nivel
+        this.obstacles = [];
+        console.log(`Nivel cargado: ${level.name}`);
     }
+    resetLevelState() {
+        this.door = null;
+        this.key = null;
+        this.palanca = null;
+        this.obstacles = [];
+    }
+
+    nextLevel() {
+        const next = this.levelIndex + 1;
+
+        if (next >= levelData.levels.length) {
+            console.log("No hay más niveles");
+            return false;
+        }
+
+        this.levelIndex = next;
+        this.loadLevel(next);
+        return true;
+    }      
+        
+    
+
 }
 
 module.exports = World;
