8f5df9ecba1984a202ac09cc2275a939bea1a9eb
Yessc <dianacarbob@gmail.com>
2026-05-05 11:26:02 +0200

    update layer

--- game/players.js
+++ game/players.js
@@ -9,6 +9,7 @@ class Player {
         // POSICIÓN: Viene de los cálculos de tu Sala
         this.x = spawnX;
         this.y = spawnY;
+
         // hitbox de los jugadores
         this.width = 30;
         this.height = 90;
@@ -20,9 +21,15 @@ class Player {
         this.finished = false;
         this.input = { left: false, right: false, jump: false };
         this.onGround = false;
-        
     }
- 
+
+    // colisión AABB
+    checkCollision(rect1, rect2) {
+        return rect1.x < rect2.x + rect2.width &&
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.y + rect1.height > rect2.y;
+    }
 
     setColor(color) {
         this.color = color;
@@ -34,27 +41,9 @@ class Player {
         if (this.finished) {
             this.vx = 0;
             this.vy = 0;
-            /*this.input.left = false;
-            this.input.right = false;
-            this.input.jump = false;*/
             return;
         }
 
-                    //revisar para mejorar la friccion
-                    /*const accel = 0.5;
-            const maxSpeed = 5;
-            const friction = 0.8;
-
-            if (this.input.left) this.vx -= accel;
-            if (this.input.right) this.vx += accel;
-
-            // límite velocidad
-            this.vx = Math.max(-maxSpeed, Math.min(maxSpeed, this.vx));
-
-            // fricción
-            if (!this.input.left && !this.input.right) {
-                this.vx *= friction;
-            }*/ 
         //SERVER: Implementació dels salts i col·lisions entre usuaris (“s’apilen” un sobre l’altre) 
         const speed = 5;
         const gravity = 0.8;
@@ -73,53 +62,66 @@ class Player {
 
         // --- FÍSICA ---
         this.vy += gravity;
+
+        // =========================
+        // MOVIMIENTO X + COLISION
+        // =========================
         this.x += this.vx;
+
+        for (const obs of this.world.obstacles) {
+            if (this.checkCollision(this, obs)) {
+                if (this.vx > 0) {
+                    this.x = obs.x - this.width;
+                } else if (this.vx < 0) {
+                    this.x = obs.x + obs.width;
+                }
+                this.vx = 0;
+            }
+        }
+
+        // =========================
+        // MOVIMIENTO Y + COLISION
+        // =========================
         this.y += this.vy;
-        //pared izquierda
+        this.onGround = false;
+
+        for (const obs of this.world.obstacles) {
+            if (this.checkCollision(this, obs)) {
+
+                if (this.vy > 0) {
+                    // cayendo encima del suelo
+                    this.y = obs.y - this.height;
+                    this.onGround = true;
+                } else if (this.vy < 0) {
+                    // chocando con techo
+                    this.y = obs.y + obs.height;
+                }
+
+                this.vy = 0;
+            }
+        }
+
+        // =========================
+        // LIMITES DEL MUNDO
+        // =========================
         if (this.x < 0) this.x = 0;
-        //pared derecha
+
         if (this.x > this.world.width - this.width) {
             this.x = this.world.width - this.width;
         }
-        
-        this.onGround = false;
-        for (const obs of this.world.obstacles) {
-            if (this.checkCollision(this, obs)) {
-                // Si el jugador está cayendo y choca con el obstáculo
-                if (this.vy > 0 && (this.y + this.height) >= obs.y) {
-                    this.y = obs.y - this.height; // Lo ponemos justo encima
-                    this.vy = 0;
-                    this.onGround = true;
-                }
-            }
-        }
 
-        // --- SUELO CORRECTO ---
-       /* const floorY = this.worldHeight - this.height;
-        if (this.y >= floorY) {
-            this.y = floorY;
+        if (this.y > this.world.height) {
+            this.y = this.world.height - this.height;
             this.vy = 0;
             this.onGround = true;
-        }*
-
-        // --- PAREDES ---
-        if (this.x < 0) this.x = 0;
-        if (this.x > this.worldWidth - this.width) {
-            this.x = this.worldWidth - this.width;
-        }*/
-            if (this.y > this.world.height) {
-                this.y = this.world.height - this.height;
-                this.vy = 0;
-                this.onGround = true;
-            }
         }
-        // Dentro de la clase Player
-        checkCollision(rect1, rect2) {
-            return rect1.x < rect2.x + rect2.width &&
-                rect1.x + rect1.width > rect2.x &&
-                rect1.y < rect2.y + rect2.height &&
-                rect1.y + rect1.height > rect2.y;
+
+        // FIX IMPORTANTE: seguridad extra
+        if (this.y < -500) {
+            this.y = 0;
+            this.vy = 0;
+        }
     }
 }
 
-module.exports = Player
+module.exports = Player;
